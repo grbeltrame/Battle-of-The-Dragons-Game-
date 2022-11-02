a82f78e82a75871dc1f8b97af9886b2bd4ba0f9d
Giovana Beltrame <grbeltrame@gmail.com>
2022-11-02 18:53:57 -0300

    game(files): arquivos modificados e adicionados

--- movimento_dragao.py
+++ movimento_dragao.py
@@ -4,19 +4,26 @@ from PPlay.sprite import *
 from PPlay.gameimage import* 
 from PPlay.keyboard import* 
 
+global velTiro
+global delay
+global tiros 
+velTiro = 600
+delay = 0
+tiros = []
+
 def trocaDragao(atual,novo):
     dragao = novo
     dragao.x = atual.x
     dragao.y = atual.y
     return dragao
 
-def movDragao(atual,janela,teclado):
+def movDragao(atual,janela,teclado,dragaoPrefStand,dragaoPrefFlying): #avaliar uma forma de fazer ele pular sem mudar para o voando
     dragao = atual
     velDragao = 300
-    # if(teclado.key_pressed("SPACE")):
-    #     dragao = movimento_dragao.trocaDragao(dragao,dragaoFlying)
-    # if(teclado.key_pressed("left_shift")) :
-    #     dragao = movimento_dragao.trocaDragao(dragao,dragaoStand)
+    if(teclado.key_pressed("SPACE")):
+        dragao = trocaDragao(dragao,dragaoPrefFlying)
+    if(teclado.key_pressed("left_shift")) :
+        dragao = trocaDragao(dragao,dragaoPrefStand)
     if(teclado.key_pressed("RIGHT")):
         dragao.x += velDragao*janela.delta_time()  
     if(teclado.key_pressed("LEFT")):
@@ -28,23 +35,35 @@ def movDragao(atual,janela,teclado):
     return dragao
 
 def criaTiro(tiros,dragao):
-    tiro = Sprite("sprites/dificuldade.png") #mudar para foguinhos depois que tiver o sprite 
+    tiro = Sprite("sprites/personagens/fogo-2.png",4) 
+    tiro.set_total_duration(1000)
     tiro.y = dragao.y - tiro.height/2
-    tiro.x = dragao.x
+    tiro.x = dragao.x*1.5
     tiros.append(tiro)
 
 def movTiro(tiros,velTiro,janela):
     for tiro in tiros:
-        tiro.y -= velTiro*janela.delta_time()
+        tiro.x += velTiro*janela.delta_time()
+        tiro.y += velTiro*janela.delta_time()
         tiro.draw()
         if(tiro.y < 0 - tiro.height):
             tiros.remove(tiro)
+def shootFireball(teclado,dragao,janela):   #ta dando bosta
+    if(teclado.key_pressed("F") ):
+        # delay = 10
+        criaTiro(tiros,dragao)     
+    if(len(tiros)>0):        
+        movTiro(tiros,velTiro,janela)
+    # if(delay>0):
+    #     delay -= 1
 
 # def colisaoDragao(plataforma,soldado,dragao):
-#     estudar as colisoes de acordo com as plataformas, soldados e tiros de outros dragoes:
+#     estudar as colisoes de acordo com as plataformas, soldados e tiros de outros dragoes
 
-# def badDragons():
-#     criar e movimentar os dragoes pelo mapa 
+# def lessHP():
+#     tirar vida do dragao quando for atingido
 
-# def movSoldados():
-#     movimento dos soldados e dos tiros dos soldados contra o dragao, preferencialmente por IA 
+# def gameOver():
+#     termina o jogo quando o jogador morrer
+
+
