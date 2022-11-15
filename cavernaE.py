46c8c67f41853231e3dabbcce451a15a6ab24415
Ana Carolina Abreu <115514989+anacabreu@users.noreply.github.com>
2022-11-15 19:21:20 -0300

    Update cavernaE.py

--- cavernaE.py
+++ cavernaE.py
@@ -14,6 +14,16 @@ def gamePlayCavE(prefDragon,dragaoPrefFlying):
     teclado = janela.get_keyboard()
 
     fundoCavE = GameImage("sprites/caverna-estatico/caverna-estatica.jpg")
+    
+    #Plataformas interativas
+    platform1 = Sprite("sprites/caverna-estatico/Cave -Platform1.png")
+    platform1.set_position(1025, 406)
+    platform2 = Sprite("sprites/caverna-estatico/Cave -Platform2.png")
+    platform2.set_position(499, 391)
+    platform3 = Sprite("sprites/caverna-estatico/Cave -Platform3.png")
+    platform3.set_position(768, 481)
+    platform4 = Sprite("sprites/caverna-estatico/Cave -Platform4.png")
+    platform4.set_position(118, 276)
 
     #Define dragão de acordo com o selecionado e com a movimentação e posiciona no inicio do jogo
     dragaoPrefStand = prefDragon
@@ -27,6 +37,10 @@ def gamePlayCavE(prefDragon,dragaoPrefFlying):
     #Game loop
     while(True):
         fundoCavE.draw()
+        platform1.draw()
+        platform2.draw()
+        platform3.draw()
+        platform4.draw()
         dragao = movimento_dragao.movDragao(dragao,janela,teclado,dragaoPrefStand,dragaoPrefFlying) 
         # fire = movimento_dragao.shootFireball(teclado,dragao,janela)      
         dragao.update()
